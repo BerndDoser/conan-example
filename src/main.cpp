b97e0612c8d8d8e49beb0742752eeee71036dd02
Bernd Doser <bernd.doser@h-its.org>
2019-11-29 16:12:15 +0100

    add range-v3 example

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <string>
+#include <vector>
+
+#include "range/v3/view/filter.hpp"
+#include "range/v3/view/transform.hpp"
 
 int main()
 {
-    std::cout << "Hello" << std::endl;
+    std::vector<int> const vi{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    using namespace ranges;
+    auto rng = vi | views::filter([](int i) { return i % 2 == 0; }) |
+               views::transform([](int i) { return std::to_string(i); });
+    std::cout << rng << std::endl;
 }
